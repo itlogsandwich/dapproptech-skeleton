afffd6e07b1f8138571eea436e35f6b403a2b7dc
Jan Hansen <tiujanhansen@gmail.com>
2026-01-23 20:21:17 +0800

    refactor: modified controllers, this may no longer work on the skeleton. this is currently being used as a seeder for the main prod backend

--- dapproptech-backend/app/controllers/agents_controller.ts
+++ dapproptech-backend/app/controllers/agents_controller.ts
@@ -10,19 +10,33 @@ export default class AgentsController {
     .whereHas('roles', (rolesQuery) => {
         rolesQuery.where('name', RoleType.AGENT)
     })
+    .withCount('listings')
     .orderBy('fullName', 'asc')
-    .exec()
 
-    return response.ok(agents)
+    const formattedAgents = agents.map(agent => {
+      return {
+        ...agent.serialize(),
+        listings: agent.$extras.listings_count,
+      }
+    })
+    return response.ok(formattedAgents)
   }
+
   public async show({ params, response }: HttpContext)
   {
     try
     {
       const agent = await User.query()
-          .where('id', params.id).firstOrFail();
+          .where('id', params.id)
+          .withCount('listings')
+          .preload('listings')
+          .firstOrFail()
 
-      return response.ok(agent)
+      return response.ok({
+        ...agent.serialize(),
+        listingsCount: agent.$extras.listings_count,
+        listings: agent.listings,
+      })
     }
     catch (error)
     {
--- dapproptech-backend/app/controllers/properties_controller.ts
+++ dapproptech-backend/app/controllers/properties_controller.ts
@@ -4,13 +4,16 @@ import Property from '#models/property'
 
 export default class PropertiesController {
   public async index(){
-    return await Property.query().orderBy('createdAt', 'desc');
+    return await Property.query()
+        .preload('agent')
+        .orderBy('createdAt', 'desc');
   }
 
   public async featuredIndex()
   {
     return await Property.query()
           .where('isFeatured', true)
+          .preload('agent')
           .orderBy('createdAt', 'desc');
   }
 
